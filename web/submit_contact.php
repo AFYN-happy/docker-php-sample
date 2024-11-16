62b3a42acab9a5b547d49c5e3bab7031b9f70919
AFYN-happy <puntyo2323@gmail.com>
2024-11-17 07:35:37 +0900

    form追加

---
+++ web/submit_contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Us Submission</title>
+</head>
+<body>
+    <h1>Thank you!</h1>
+    <?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    echo "Thank you, Name: $name<br>";
+    ?>
+</body>
+</html>
